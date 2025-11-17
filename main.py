33c678640d1a542eb60f29c3decc5b9b22ff844e
quollcucumber <rgithub@greatcactus.org>
2025-11-17 18:23:36 +1100

    Update main.py

--- main.py
+++ main.py
@@ -50,6 +50,8 @@ background = resize_image(background, 650)
 while running:
     screen.fill((255, 255, 255))
     screen.blit(background, (0, 0))
+    text = font.render("EASY                              MEDIUM                           HARD", True, (255, 255, 255))
+    screen.blit(text, (100, 150))
     dificultytext = font.render("Choose dificulty", True, (255, 255, 255))
     screen.blit(dificultytext, (330, 10))
     # TODO Hadrien make better easy medium hard images
@@ -89,23 +91,26 @@ elif dif == 3:
     cols = 20
 
 grid = []
-for i in range(rows * cols):
-    if random.randint(0, 5) == 0:
-        grid.append(10)
-    else:
-        grid.append(0)
+for i in range(rows):
+    a = []
+    for j in range(cols):
+        if random.randint(0, 7) == 0:
+            a.append(10)
+        else:
+            a.append(0)
+    grid.append(a)
 
 
 def find(row, col):
     if row >= rows or row < 0 or col >= cols or cols < 0:
         return 0
-    return grid[row * rows + col]
+    return grid[row][col]
 
 
 def edit(row, col, setval):
     if row >= rows or row < 0 or col >= cols or cols < 0:
         return
-    grid[row * rows + col] = setval
+    grid[row][col] = setval
     return
 
 
@@ -136,21 +141,57 @@ for row in range(rows):
         a.append(find(row, col))
     print(a)
 
-
 tile = pygame.image.load("Minesweeper Tile.jpg")
-tile_height = 400 / rows
+tile_height = 400 / rows + 1
 tile = resize_image(tile, tile_height)
 flagtile = pygame.image.load("Minesweeper Flag.png")
 flagtile = resize_image(flagtile, tile_height)
+neighbor0 = pygame.image.load("Minesweeper 0.jpg")
+neighbor0 = resize_image(neighbor0, tile_height)
 neighbor1 = pygame.image.load("Minesweeper 1.png")
 neighbor1 = resize_image(neighbor1, tile_height)
-neighbor2 = pygame.image.load("Minesweeper 1.png")
+neighbor2 = pygame.image.load("Minesweeper 2.png")
 neighbor2 = resize_image(neighbor2, tile_height)
-neighbor3 = pygame.image.load("Minesweeper 1.png")
+neighbor3 = pygame.image.load("Minesweeper 3.png")
 neighbor3 = resize_image(neighbor3, tile_height)
-neighbor4 = pygame.image.load("Minesweeper 1.png")
+neighbor4 = pygame.image.load("Minesweeper 4.png")
 neighbor4 = resize_image(neighbor4, tile_height)
+neighbor5 = pygame.image.load("Minesweeper 5.png")
+neighbor5 = resize_image(neighbor5, tile_height)
+neighbor6 = pygame.image.load("Minesweeper 6.png")
+neighbor6 = resize_image(neighbor6, tile_height)
+neighbor7 = pygame.image.load("Minesweeper 7.png")
+neighbor7 = resize_image(neighbor7, tile_height)
+neighbor8 = pygame.image.load("Minesweeper 8.png")
+neighbor8 = resize_image(neighbor8, tile_height)
 flags = []
+q = []
+opened = []
+for i in range(rows):
+    a = []
+    for j in range(cols):
+        a.append(0)
+    opened.append(a)
+
+
+def bfs():
+    while len(q) != 0:
+        row, col = q[len(q) - 1]
+        q.pop()
+        if 0 <= row < rows and 0 <= col < cols:
+            if opened[row][col] != 1:
+                opened[row][col] = 1
+                if find(row, col) == 0:
+                    q.append((row + 1, col))
+                    q.append((row + 1, col - 1))
+                    q.append((row + 1, col + 1))
+                    q.append((row, col + 1))
+                    q.append((row, col - 1))
+                    q.append((row - 1, col + 1))
+                    q.append((row - 1, col))
+                    q.append((row - 1, col - 1))
+
+
 for i in range(rows):
     a = []
     for j in range(cols):
@@ -161,14 +202,40 @@ while running:
     screen.fill((255, 255, 255))
     screen.blit(background, (0, 0))
     for i in range(rows):
+        prints = []
         for j in range(cols):
-            if(flags[i][j] == 0):
-                screen.blit(tile, (300 + i * 400 / rows, 100 + j * 400 / cols))
+            a = find(i, j)
+            prints.append(a)
+            pos = (300 + j * 400 / cols, 100 + i * 400 / rows)
+            if flags[i][j] == 0:
+                # if opened[i][j] == 0:
+                if opened[i][j] == 0:
+                    screen.blit(tile, pos)
+                else:
+                    if find(i, j) == 0:
+                        screen.blit(neighbor0, pos)
+                    elif find(i, j) == 1:
+                        screen.blit(neighbor1, pos)
+                    elif find(i, j) == 2:
+                        screen.blit(neighbor2, pos)
+                    elif find(i, j) == 3:
+                        screen.blit(neighbor3, pos)
+                    elif find(i, j) == 4:
+                        screen.blit(neighbor4, pos)
+                    elif find(i, j) == 5:
+                        screen.blit(neighbor5, pos)
+                    elif find(i, j) == 6:
+                        screen.blit(neighbor6, pos)
+                    elif find(i, j) == 7:
+                        screen.blit(neighbor7, pos)
+                    elif find(i, j) == 8:
+                        screen.blit(neighbor8, pos)
             else:
-                screen.blit(flagtile, (300 + i * 400 / rows, 100 + j * 400 / cols))
-    if (dif == 1):
+                screen.blit(flagtile, pos)
+        # print(prints)
+    if dif == 1:
         dificultytext = font.render("EASY", True, (255, 255, 255))
-    elif (dif == 2):
+    elif dif == 2:
         dificultytext = font.render("MEDIUM", True, (255, 255, 255))
     else:
         dificultytext = font.render("HARD", True, (255, 255, 255))
@@ -176,20 +243,30 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if (event.type == pygame.MOUSEBUTTONDOWN):
-            if(event.button == 1):
-                #left click, click a grid to open
-                # running = False;
-                b = 0
-            elif(event.button == 3):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                pos = pygame.mouse.get_pos()
+                row = int((pos[1] - 100) / int(400 / cols))
+                col = int((pos[0] - 300) / int(400 / rows))
+                if 0 <= row < rows and 0 <= col < cols:
+                    q.append((row, col))
+                    bfs()
+                    print(row, col)
+                    if find(row, col) == 10:
+                        print("YOU DIED")
+                        running = False
+
+            elif event.button == 3:
                 #right click, add/remove a flag
                 pos = pygame.mouse.get_pos()
-                col = int((pos[1] - 100) /int ( 400 / cols))
-                row = int((pos[0] - 300) / int(400 / rows))
-                flags[row][col] = 1 - flags[row][col]
+                row = int((pos[1] - 100) / int(400 / cols))
+                col = int((pos[0] - 300) / int(400 / rows))
+                if 0 <= row < rows and 0 <= col < cols:
+                    flags[row][col] = 1 - flags[row][col]
                 print("right")
     pygame.display.flip()
     clock.tick(60)
+    # running = False
 
 pygame.quit()
 
